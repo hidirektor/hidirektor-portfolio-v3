0adf3b28eea386da44efdeb99867e944cd04d91f
hidirektor <hidirektor@gmail.com>
2026-06-02 22:10:45 +0300

    General UI Update

--- app/components/LinkedInSection.tsx
+++ app/components/LinkedInSection.tsx
@@ -76,7 +76,7 @@ export default function LinkedInSection() {
             </div>
           </div>
         ) : posts.length === 0 ? (
-          <div className="flex flex-col items-center justify-center py-20 pt-10 gap-6 text-center">
+          <div className="flex flex-col items-center justify-center gap-8 text-center pt-16 pb-20">
             <div className="w-20 h-20 rounded-full bg-[#0077b5]/10 flex items-center justify-center text-[#0077b5] mb-2">
               <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" viewBox="0 0 24 24" fill="currentColor"><path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"/></svg>
             </div>
